d7cde8a95e7f8e584d52d8e07255f6e862781927
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-02-23 02:09:31 +0300

    fixed missing admin-search error

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('search/s',['as'=>'search','uses'=>'Ogilo\Search\Http\Controllers\SearchController@getSearch']);
 
-Route::group(['middleware'=>'web','namespace'=>'Ogilo\AdminMd\Http\Controllers\Web','prefix'=>'admin','as'=>'admin'],function(){
-    Route::get('',['as'=>'-search','uses'=>'SearchController@search']);
-});
+// Route::group(['middleware'=>'web','namespace'=>'Ogilo\AdminMd\Http\Controllers\Web','prefix'=>'admin','as'=>'admin'],function(){
+//     Route::get('',['as'=>'-search','uses'=>'SearchController@search']);
+// });
